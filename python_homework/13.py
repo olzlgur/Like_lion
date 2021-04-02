9a91bab71fa73be595130c012317cba7a1ce9434
Lee Ji Heok <olzlgur@naver.com>
2021-04-02 23:08:42 +0900

    2021-04-02

---
+++ python_homework/13.py
@@ -0,0 +1,15 @@
+list1 = [1, 2, 3, 4]
+
+# 아래줄에서 list1의 1번째 자리에 8을 넣고 원래 있던 값은 오른쪽으로 밀어 보세요.
+list1.insert(1,8)
+
+print("첫 번째 자리에 8을 넣은 결과 : {}".format(list1))
+# 아래줄에서 list1을 작은 수부터 큰 수로 정렬해 보세요
+list1.sort()
+
+print("list1을 작은 수부터 큰 수로 정렬한 결과 : {}".format(list1))
+# 아래줄에서 list1을 거꾸로 만들어 보세요
+list1.reverse()
+
+
+print("list1을 거꾸로 정렬한 결과 : {}".format(list1))
