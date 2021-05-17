688c3eb77611da8ffba4f961381644240e1a24ec
Lee Ji Heok <olzlgur@naver.com>
2021-05-17 17:16:38 +0900

    2021-05-17

--- session_blog/blogproject/blogproject/urls.py
+++ session_blog/blogproject/blogproject/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('detail<str:id>', views.detail, name="detail"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('edit/<str:id>', views.edit, name="edit"),
+    path('update/<str:id>', views.update, name="update"),
 ]
