2ba9854018bd538e32ce147908c0bc5e17c39476
Lee Ji Heok <olzlgur@naver.com>
2021-05-17 17:17:19 +0900

    2021-05-17

--- session_blog/blogproject/blog/views.py
+++ session_blog/blogproject/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Blog
+from django.utils import timezone
 # Create your views here.
 
 def home(request):
@@ -9,3 +10,28 @@ def home(request):
 def detail(request, id):
   blog = get_object_or_404(Blog, pk=id)
   return render(request, 'detail.html', {'blog' : blog}) 
+
+def new(request):
+  return render(request,'new.html')
+
+def create(request):
+  new_blog = Blog()
+  new_blog.title = request.POST['title']
+  new_blog.writer= request.POST['writer']
+  new_blog.pub_date=timezone.now()
+  new_blog.body= request.POST['body']
+  new_blog.save()
+  return redirect('detail',new_blog.id)
+
+def edit(request, id):
+  edit.blog = Blog.objects.get(id=id)
+  return render(request, 'edit.html', {'blog':edit.blog})
+
+def update(request, id):
+  update_blog = Blog.objects.get(id=id)
+  update_blog.title = request.POST['title']
+  update_blog.writer= request.POST['writer']
+  update_blog.pub_date=timezone.now()
+  update_blog.body= request.POST['body']
+  update_blog.save()
+  return redirect('detail',update_blog.id)
