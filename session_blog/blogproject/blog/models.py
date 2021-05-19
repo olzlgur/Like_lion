eaf427e7d7754e18d9b45aea3c775e1abcc0e5bc
Lee Ji Heok <olzlgur@naver.com>
2021-05-19 23:37:02 +0900

    2021-05-19

--- session_blog/blogproject/blog/models.py
+++ session_blog/blogproject/blog/models.py
@@ -6,6 +6,7 @@ class Blog(models.Model):
   title = models.CharField(max_length=200)
   writer = models.CharField(max_length=100)
   pub_date = models.DateTimeField()
+  image = models.ImageField(upload_to="blog/", blank=True)
   body = models.TextField()
 
   def __str__(self):
