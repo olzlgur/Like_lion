5ab5e514aa564a55dff40fad126731bf2d0765f5
Lee Ji Heok <olzlgur@naver.com>
2021-06-28 23:25:23 +0900

    2021-06-28

---
+++ skeleton_code/bp/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField('data published')
+    body = models.TextField()
+
+    writer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    body = models.TextField(max_length = 500)
+    pub_date = models.DateTimeField('data published')
+    writer = models.ForeignKey(User, on_delete = models.CASCADE)
+    post = models.ForeignKey(Blog, on_delete = models.CASCADE)
